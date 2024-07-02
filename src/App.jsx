4b0d23575f218e45a1cf4aca8f5859e494dccb4f
hemanthcse1 <hemanthcse1@gmail.com>
2024-07-02 10:05:38 +0530

    using useEffect react hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import MyComponent from "./MyComponent";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <h1>React Hooks</h1>
+      <MyComponent />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/MyComponent.jsx
@@ -0,0 +1,33 @@
+import Reac, { useState, useEffect } from "react";
+
+export default function MyComponent() {
+  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("blue");
+
+
+  useEffect(() => {
+    document.title = `Count ${count} ${color}`;
+  }, [count, color]);
+
+  function addCount() {
+    setCount((c) => c + 1);
+  }
+
+  function subtractCount() {
+    setCount((c) => c - 1);
+  }
+
+  function changeColor() {
+    setColor((c) => (c === "blue" ? "red" : "blue"));
+  }
+
+  return (
+    <div>
+      <p style={{ color: color }}>Count: {count}</p>
+      <button onClick={addCount}>Click</button> <br></br>
+      <button onClick={subtractCount}>Subtract</button> <br></br>
+      <button onClick={changeColor}>Change Color</button>
+    
+    </div>
+  );
+}
